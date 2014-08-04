353fef171589b76a102e93f7840a238bb3482492
Jonas Friedmann <j@frd.mn>
2014-08-04 17:28:27 +0200

    Improve Array

--- pia.sh
+++ pia.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
 # Set array of PIA network locations
-piaNetworks=('
-	us-midwest.privateinternetaccess.com
-	us-east.privateinternetaccess.com
-	us-texas.privateinternetaccess.com
-	us-west.privateinternetaccess.com
-	us-california.privateinternetaccess.com
-	us-seattle.privateinternetaccess.com
-	us-florida.privateinternetaccess.com
-	ca.privateinternetaccess.com
-	ca-toronto.privateinternetaccess.com
-	uk-london.privateinternetaccess.com
-	uk-southampton.privateinternetaccess.com
-	swiss.privateinternetaccess.com
-	nl.privateinternetaccess.com
-	sweden.privateinternetaccess.com
-	france.privateinternetaccess.com
-	germany.privateinternetaccess.com
-	romania.privateinternetaccess.com
-	hk.privateinternetaccess.com
-')
+piaNetworks=(
+	'us-midwest.privateinternetaccess.com' 
+	'us-east.privateinternetaccess.com' 
+	'us-texas.privateinternetaccess.com' 
+	'us-west.privateinternetaccess.com' 
+	'us-california.privateinternetaccess.com' 
+	'us-seattle.privateinternetaccess.com' 
+	'us-florida.privateinternetaccess.com' 
+	'ca.privateinternetaccess.com' 
+	'ca-toronto.privateinternetaccess.com' 
+	'uk-london.privateinternetaccess.com' 
+	'uk-southampton.privateinternetaccess.com' 
+	'swiss.privateinternetaccess.com' 
+	'nl.privateinternetaccess.com' 
+	'sweden.privateinternetaccess.com' 
+	'france.privateinternetaccess.com' 
+	'germany.privateinternetaccess.com' 
+	'romania.privateinternetaccess.com' 
+	'hk.privateinternetaccess.com'
+)
+
+echo "Attempting to add '${#piaNetworks[@]}' PrivateInternetAccess VPNs"
 
 # For each network, execute addVPN.scpt
 for network in ${piaNetworks}
